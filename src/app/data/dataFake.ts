a3c2d1544a1de20d4c1f78ca9e195e8ad63a9146
glaub <glaubero.souza@gmail.com>
2023-10-07 21:12:51 -0300

    Content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO EM 3D',
+    description: 'Marvel anuncia um novo filme do homem de ferro',
+    photoCover:
+      'https://is5-ssl.mzstatic.com/image/thumb/toO8_a7YKPIpfUgyJ7MnYg/1200x675mf.jpg',
+  },
+
+  {
+    id: '2',
+    title: 'NOVA SERIE ANUNCIADA NA DISNEY +',
+    description: 'A data está muito próxima',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+];
